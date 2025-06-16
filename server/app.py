8c6d30324035d6b308a7992abd586d4e0b229e7a
Techbandit001 <davidkiptum80@gmail.com>
2025-06-16 23:05:02 +0300

    addss flask

--- server/app.py
+++ server/app.py
@@ -1,27 +1,30 @@
 # server/app.py
-#!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, jsonify
 from flask_migrate import Migrate
-
 from models import db, Earthquake
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
 
-migrate = Migrate(app, db)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 db.init_app(app)
-
+migrate = Migrate(app, db)
 
 @app.route('/')
 def index():
-    body = {'message': 'Flask SQLAlchemy Lab 1'}
-    return make_response(body, 200)
+    return {"message": "Earthquake API"}
 
-# Add views here
+@app.route('/earthquakes/<int:id>')
+def get_earthquake_by_id(id):
+    earthquake = Earthquake.query.filter_by(id=id).first()
+    if earthquake:
+        return jsonify(earthquake.to_dict()), 200
+    return jsonify({"message": f"Earthquake {id} not found."}), 404
 
-
-if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def get_earthquakes_by_magnitude(magnitude):
+    quakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    quake_list = [quake.to_dict() for quake in quakes]
+    return jsonify({"count": len(quake_list), "quakes": quake_list}), 200
